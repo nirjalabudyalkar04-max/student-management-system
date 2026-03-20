d516ceb18ce44ce5de8069b11308c96f9dee9d8e
nirjalabudyalkar04-max <nirjalabudyalkar04@gmail.com>
2026-03-20 15:00:19 +0530

    Update StudentNoticeBoard.jsx

--- client/src/components/studentLayout/StudentNoticeBoard.jsx
+++ client/src/components/studentLayout/StudentNoticeBoard.jsx
@@ -7,7 +7,7 @@ const StudentNoticeBoard = () => {
   useEffect(() => {
     const fetchNotices = async () => {
       try {
-        const response = await fetch("https://student-management-system-8hjo.onrender.com");
+        const response = await fetch("https://student-management-system-hla4.onrender.com");
         const data = await response.json();
 
         if (Array.isArray(data)) {
