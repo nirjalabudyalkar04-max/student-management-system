6c6d90d99c3293292aebced2e1c33a1d9634f9f0
nirjalabudyalkar04-max <nirjalabudyalkar04@gmail.com>
2026-03-20 14:59:17 +0530

    Update Register.jsx

--- client/src/components/guestLayout/Register.jsx
+++ client/src/components/guestLayout/Register.jsx
@@ -33,7 +33,7 @@ const Register = () => {
     setIsLoading(true);
     try {
       const res = await axios.post(
-        "https://student-management-system-8hjo.onrender.com",
+        "https://student-management-system-hla4.onrender.com",
         formData
       );
 
